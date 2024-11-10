55095f3c5f8dda846b7a5ce0a69939c76ac53b11
Adithyakb@2005 <adithyakb1001@gmail.com>
2024-11-11 01:13:55 +0530

    react hook

---
+++ rhooks/src/App.jsx
@@ -0,0 +1,141 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+// import { useState } from "react"
+// import ReactDOM from "react-dom/client"
+
+// function Component1() {
+//   const [user, setUser] = useState("Jesse Hall");
+
+//   return (
+//     <>
+//       <h1>{`Hello ${user}!`}</h1>
+//       <Component2 user={user} />
+//     </>
+//   );
+// }
+
+// function Component2({ user }) {
+//   return (
+//     <>
+//       <h1>Component 2</h1>
+//       <Component3 user={user} />
+//     </>
+//   );
+// }
+
+// function Component3({ user }) {
+//   return (
+//     <>
+//       <h1>Component 3</h1>
+//       <Component4 user={user} />
+//     </>
+//   );
+// }
+
+// function Component4({ user }) {
+//   return (
+//     <>
+//       <h1>Component 4</h1>
+//       <Component5 user={user} />
+//     </>
+//   );
+// }
+
+// function Component5({ user }) {
+//   return (
+//     <>
+//       <h1>Component 5</h1>
+//       <h2>{`Hello ${user} again!`}</h2>
+//     </>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Component1 />);
+
+// export default App
+
+
+
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+  const calculation = expensiveCalculation(count);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = () => {
+    setTodos((t) => [...t, "New Todo"]);
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>My Todos</h2>
+        {todos.map((todo, index) => {
+          return <p key={index}>{todo}</p>;
+        })}
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+        <h2>Expensive Calculation</h2>
+        {calculation}
+      </div>
+    </div>
+  );
+};
+
+const expensiveCalculation = (num) => {
+  console.log("Calculating...");
+  for (let i = 0; i < 1000000000; i++) {
+    num += 1;
+  }
+  return num;
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+export default App
